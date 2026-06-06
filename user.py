67d14c96976b6ce9152f6dfad5398dffe3cc9181
sasivarman-s <sureshraman2680@gmail.com>
2026-06-06 09:49:00 +0530

    content written

--- user.py
+++ user.py
@@ -1,1 +1,9 @@
+admin="admin1"
+password="1234567890"
 
+a=str(input("Enter your username: "))
+p=str(input("Enter your password: "))
+if a==admin and p==password:
+    print("Login successful!")  
+ else:
+    print("Login failed. Please check your username and password.")   
