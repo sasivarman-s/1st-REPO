38e64e332c2fe3a6fe53b0a8fdc3ed4a2ff04a96
sasivarman-s <sureshraman2680@gmail.com>
2026-06-06 09:37:37 +0530

    try except added

--- vowel.py
+++ vowel.py
@@ -1,6 +1,11 @@
-text=str(input("Enter a text: "))
-count=0
-for i in text:
-    if i in "aeiouAEIOU":
-        count= count+1
-print("Number of vowels:", count)
+try:
+    text=str(input("Enter a text: "))
+    count=0
+    for i in text:
+        if i in "aeiouAEIOU":
+            count= count+1
+    print("Number of vowels:", count)
+except ValueError:
+    print("An error occurred.")
+finally:
+    print("Program ended.")        
